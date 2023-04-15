7cc301eec03e4d70a848d16db4de06f5825dc7cf
jesusperezTFG <76495835+jesusperezTFG@users.noreply.github.com>
2023-04-15 19:47:34 +0200

    nueva config gdp 10

--- src/index.js
+++ src/index.js
@@ -8,5 +8,10 @@ app.use(cors())
 app.use(express.json());
 app.use('/api', require('./routes/userRoutes'));
 
-app.listen(8080);
-console.log("Server on port ", 8080);
+//app.listen(8080);
+//console.log("Server on port ", 8080);
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+  });
